5013b3a293862223a812727b376da4948d18eb4f
materiasipm <62041344+materiasipm@users.noreply.github.com>
2021-05-14 12:29:10 -0300

    Create ejemplo14deMayo.js

---
+++ taller5to/ejemplo14deMayo.js
@@ -0,0 +1,7 @@
+  /*
+https://developer.mozilla.org/es/docs/Web/API/Element/getBoundingClientRect
+ */
+
+let cuadrado = document.getElementById("cuadrado");
+let posicionXdeCuadrado = cuadrado.getBoundingClientRect().x;
+let posicionYdeCuadrado = cuadrado.getBoundingClientRect().y;
