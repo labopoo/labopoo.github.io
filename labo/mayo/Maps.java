1662eaaa2cd83f29f8d2503e860725f46e44c5cf
materiasipm <62041344+materiasipm@users.noreply.github.com>
2021-05-17 13:54:37 -0300

    Create Maps.java

---
+++ labo/mayo/Maps.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Maps {
+
+    public static void main(String[] args) {
+
+        HashMap<String,String> diccionario = new HashMap<>();
+
+        diccionario.put("ala","Cada uno de los órganos o apéndices pares que utilizan algunos animales para volar.");
+        diccionario.put("barra","Pieza de metal u otra materia, de forma generalmente prismática o cilíndrica y mucho más larga que gruesa");
+
+        for ( String key : diccionario.keySet() ){
+            String valor = diccionario.get(key);
+            System.out.println(valor);
+        }
+
+        HashMap<String,Integer> palabraConNumero = new HashMap<>();
+        palabraConNumero.put("uno",1);
+        palabraConNumero.put("dos",2);
+        palabraConNumero.put("tres",3);
+        palabraConNumero.put("cuatro",4);
+
+        palabraConNumero.values();
+
+        palabraConNumero.containsValue(3);
+
+        palabraConNumero.containsKey("tres");
+
+        palabraConNumero.remove("tres");
+
+        palabraConNumero.clear();
+
+        palabraConNumero.isEmpty();
+
+        palabraConNumero.size();
+
+        HashMap<String,Float> preciosDeProductos = new HashMap<>();
+        preciosDeProductos.put("fideos",56.45f);
+        preciosDeProductos.put("puré de tomates",30.55f);
+        preciosDeProductos.put("yogur",30.55f);
+
+        System.out.println(preciosDeProductos);
+        //{puré de tomates=30.55, yogur=30.55, fideos=56.45}
+
+    }
+}
