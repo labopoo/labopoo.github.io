c246541d9b9ca0dd471998d2fd8485640b6be249
materiasipm <62041344+materiasipm@users.noreply.github.com>
2021-07-01 08:18:22 -0300

    Create ejemplos1deJulio.py

---
+++ taller6to/ejemplos1deJulio.py
@@ -0,0 +1,23 @@
+# ejemplo 1
+def convertir_en_minusculas(valor):
+  if type(valor) == str:
+    return valor.lower()
+  else:
+    return valor
+
+# ejemplo 2
+def arreglar_fecha(valor): 
+  valor = str(valor)
+  lista = ['\\\\','\\','-', ' ','//']
+  for caracter in valor:
+    if not caracter.isnumeric() and caracter != "/"
+      valor = valor.replace(caracter,'/')
+  return valor
+
+# applymap, realiza la modificación en todos los elementos del dataframe
+df2 = df1.applymap(convertir_en_minusculas)
+
+# apply, realiza la modificación sólo en los elementos de una columna
+df3 = df1['nombre_de_columna'] = df_2['nombre_de_columna'].apply(convertir_en_minusculas)
+
+df4['Precio'] = df1['Precio'].apply(float)
